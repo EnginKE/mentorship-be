3c035cb9a6457aaca2d8528f2d2616dd3dfaeb3b
Engin <e_n_gin@hotmail.com>
2020-05-15 18:56:32 +0300

    first commit

---
+++ src/test/java/com/obss/mentorship/MentorshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obss.mentorship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
